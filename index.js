699a2bbbd314a3156bed1b031ee9651c845fb47c
mbokotho <mplwando@gmail.com>
2018-08-23 11:55:05 +0200

    updated stly

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const moment = require('moment');
 
 const flash = require('express-flash');
 
- const session = require('express-session');
+const session = require('express-session');
 
 const Greeting = require('./Greet');
 const Set = Greeting();
@@ -18,68 +18,71 @@ const Set = Greeting();
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
-  helpers:
-   {
-    "timestamp": function()
-    {
+  helpers: {
+    "timestamp": function() {
       return moment(this.timestamp).fromNow();
     }
   }
 }));
 
 app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
 app.use(session({
-   secret : "<add a secret string here>",
-   resave: false,
-   saveUninitialized: true
- }));
+  secret: "<add a secret string here>",
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(flash());
 
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
 
-const greet = Set.mygreeting();
-const count = Set.myCounter();
+  const greet = Set.mygreeting();
+  const count = Set.myCounter();
 
-res.render('home', {greet,count
+  res.render('home', {
+    greet,
+    count
+
+  });
 
 });
 
-});
-
-app.post('/greetings', function(req, res){
+app.post('/greetings', function(req, res) {
 
 
   const Name = req.body.Name;
   const language = req.body.language;
 
-Set.myGreet(language,Name);
+  Set.myGreet(language, Name);
 
 
-if (Name === '' || language == null) {
-  req.flash('info', 'Please Enter a Name and Select a Language !')
-} else {
-  Set.myGreet(language,Name);
-}
+  if (Name === '' || language == null) {
+    req.flash('info', 'Please Enter a Name and Select a Language !')
+  } else {
+    Set.myGreet(language, Name);
+    // let  Set.myCounter() = 0;
+  }
 
   res.redirect('/');
 
 });
 
-app.post('/reset', function(req, res){
+app.post('/reset', function(req, res) {
   Set.resetFunction();
-res.redirect('/');
+  res.redirect('/');
 });
 
 let PORT = process.env.PORT || 3002;
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
   console.log('App starting on port', PORT);
 });
