1d4ea78089f1b6a0ff2bf8afdb11b56ac168938d
mbokotho <mplwando@gmail.com>
2018-09-10 10:06:41 +0200

    FIXED ESLINT FOR GREETINGS

--- routes/greetings.js
+++ routes/greetings.js
@@ -1,85 +1,72 @@
 let greetService = require('../services/greeting');
 
-module.exports = function(pool) {
- let getGreet =  greetService(pool);
+module.exports = function (pool) {
+    let getGreet = greetService(pool);
 
-    async function home(req, res) {
-
-      try{
-          let count = await getGreet.allCount();
-          res.render('home', {count});
+    async function home (req, res) {
+        try {
+            let count = await getGreet.allCount();
+            res.render('home', { count });
+        } catch (err) {
         }
-      catch(err){
-      }
+    }
 
-}
+    async function greetings (req, res) {
+        try {
+            const Name = req.body.Name;
+            const language = req.body.language;
 
-async function greetings(req, res) {
-  try{
-    const Name = req.body.Name;
-    const language = req.body.language;
+            let Person = Name.toUpperCase();
 
-    let Person = Name.toUpperCase();
+            if (Name === '' && language === undefined) {
+                req.flash('info', 'Please Enter a Name and Select a Language !');
+            } else if (Name === '') {
+                req.flash('info', 'Please Enter a Name ');
+            } else if (language === undefined) {
+                req.flash('info', 'Please Select a Languge');
+            } else if (isNaN(Person)) {
+                await getGreet.greeter(Person, language);
+                req.flash('info', language + ', ' + Person);
+            }
 
-    if (Name === '' && language === undefined) {
-      req.flash('info', 'Please Enter a Name and Select a Language !')
+            res.redirect('/');
+        } catch (err) {
 
-    } else if (Name === '') {
-      req.flash('info', 'Please Enter a Name ')
+        }
+    }
 
-    } else if (language === undefined) {
-      req.flash('info', 'Please Select a Languge')
+    async function greeted (req, res) {
+        try {
+            let names = await getGreet.readData();
+            res.render('greeted', { names });
+        } catch (err) {
 
-    } else if(isNaN(Person)){
-      await getGreet.greeter(Person,language);
-     req.flash('info', language + ", " + Person );
+        }
+    }
+    async function counter (req, res) {
+        try {
+            let username = req.params.username;
+            let results = await getGreet.readUser(username);
+            res.render('names', { times: results });
+        } catch (err) {
+            res.send(err.stack);
+        }
+    }
 
-      }
+    async function Reset (req, res) {
+        try {
+            await getGreet.reset();
+            res.redirect('/');
+        } catch (err) {
 
-    res.redirect('/');
-  }
-  catch(err){
+        }
+    }
 
-  }
-
-}
-
-async function greeted(req, res) {
-  try{
-    let names = await getGreet.readData();
-    res.render('greeted', {names});
-
-  }catch(err){
-
-  }
-
-}
-async function counter(req, res) {
-  try{
-  let username = req.params.username;
-  let results = await  getGreet.readUser(username)
-  res.render('names', {times: results });
-  } catch(err){
-    res.send(err.stack)
-  }
-}
-
-
-async function Reset(req,res) {
-  try{
-    await getGreet.reset();
-    res.redirect('/');
-  } catch(err){
-
-  }
-
-}
-
-return {
-  home,
-  greetings,
-  greeted,
-  Reset,
-  counter
-}
-}
+    return {
+        home,
+        greetings,
+        greeted,
+        Reset,
+        counter
+    };
+};
